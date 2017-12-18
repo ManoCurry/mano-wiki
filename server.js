32e7c457bbf73c0893419275bd319c08cfddf971
Woohyeon Roh <woohyeonroh@Woohyeons-MacBook-Pro.local>
2017-12-18 22:08:34 +0900

    Add routes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 
+const Article = require('./models/Article')
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
@@ -9,20 +11,58 @@ app.use(bodyParser.urlencoded({
 
 app.set('views', './views')
 app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
   res.render('index')
 })
 
-/**
- * Routes
- * - /: list
- * - /new | submit form
- * - /:postId  | post
- * - /:postId/edit  |  form
- */
+// Route
+app.get('/wiki/:title', async (req, res) => {
+  const { title } = req.params
+  const article = await Article.findOne({
+    title,
+  })
+  
+  if (article == null) {
+    res.status(404).send('Loser')
+  } else {
+    const { content } = article
+    res.render('show', {
+      title,
+      content,
+    })
+  }
+})
 
- app.get('/new', (req, res) => {
-   res.render('form')
- })
+
+// get edit
+app.get('/wiki/:title/edit', (req, res) => {
+  const { title } = req.params
+
+  res.render('form', {
+    title,
+  })
+})
+
+// post upsert
+app.post('/wiki/:title/edit', async (req, res) => {
+  const { content } = req.body
+  const { title } = req.params
+  let article = await Article.findOne({
+    title
+  })
+
+  if (article == null) {
+    article = new Article({
+      title
+    })
+  }
+
+  article.content = content
+
+  await article.save()
+
+  res.redirect(`/wiki/${title}`)
+})
 
 app.listen(8080)
